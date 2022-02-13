21aff9ae1895bea07f828937b41c4c7388529401
Juan <juanfelipeoficial99@gmail.com>
2022-02-13 17:56:54 -0300

    Adicionado Lombok e Mapper ao projeto

--- bookstoremanager/src/main/java/com/juanfelipe/bookstoremanager/controller/BookController.java
+++ bookstoremanager/src/main/java/com/juanfelipe/bookstoremanager/controller/BookController.java
@@ -1,14 +1,15 @@
 package com.juanfelipe.bookstoremanager.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.juanfelipe.bookstoremanager.dto.BookDTO;
 import com.juanfelipe.bookstoremanager.dto.MessageResponseDTO;
-import com.juanfelipe.bookstoremanager.entity.Book;
-import com.juanfelipe.bookstoremanager.repository.BookRepository;
 import com.juanfelipe.bookstoremanager.service.BookService;
 
 @RestController
@@ -26,8 +27,8 @@ public class BookController {
 
 
 	@PostMapping
-	public MessageResponseDTO create (@RequestBody Book book) {
-		return bookService.create(book);
+	public MessageResponseDTO create (@RequestBody @Valid BookDTO bookDTO) {
+		return bookService.create(bookDTO);
 	}
 	
 }
--- bookstoremanager/src/main/java/com/juanfelipe/bookstoremanager/service/BookService.java
+++ bookstoremanager/src/main/java/com/juanfelipe/bookstoremanager/service/BookService.java
@@ -2,10 +2,11 @@ package com.juanfelipe.bookstoremanager.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
+import com.juanfelipe.bookstoremanager.dto.BookDTO;
 import com.juanfelipe.bookstoremanager.dto.MessageResponseDTO;
 import com.juanfelipe.bookstoremanager.entity.Book;
+import com.juanfelipe.bookstoremanager.mapper.BookMapper;
 import com.juanfelipe.bookstoremanager.repository.BookRepository;
 
 @Service
@@ -13,13 +14,17 @@ public class BookService {
 	
 	private BookRepository bookRepository;
 	
+	private final BookMapper bookMapper = BookMapper.INSTANCE;
+	
 	@Autowired
 	public BookService(BookRepository bookRepository) {
 		this.bookRepository = bookRepository;
 	}
 	
-	public MessageResponseDTO create (Book book) {
-		Book savedBook = bookRepository.save(book);
+	public MessageResponseDTO create (BookDTO bookDTO) {
+		Book bookToSave= bookMapper.toModel(bookDTO); 
+		
+		Book savedBook = bookRepository.save(bookToSave);
 		return MessageResponseDTO.builder()
 				.message("Book created with ID "+ savedBook.getId())
 				.build();
